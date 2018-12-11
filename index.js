ae3972036dddfd42542bee40ce2a17bb03eb895f
Steve Bogucki <sbogucki@mail.usf.edu>
2018-12-10 17:35:14 -0800

    added to the authentication branch. auth didnt work

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const keys = require('./config/keys');
-require('./services/passport');
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+const db = mongoose.connection;
 
-const app = express();
+//handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  // we're connected!
+});
 
-require('./routes/authRoutes')(app);
+//use sessions for tracking logins
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// serve static files from template
+app.use(express.static(__dirname + '/templateLogReg'));
+
+// include routes
+const routes = require('./routes/router');
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = (404).toString();
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+//   res.status(err.status || (500).toString());
+  res.send(err.message);
+});
 
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT);
