f18f39ca95d6de6414d12118a98a3f50a6c26f74
Steve Bogucki <sbogucki@mail.usf.edu>
2018-12-13 15:22:49 -0800

    testing something

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>sup</code> and save to reload.
+            Edit <code>test</code> and save to reload.
           </p>
           <a
             className="App-link"
