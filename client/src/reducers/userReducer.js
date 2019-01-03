fd412fa963676eb21b4ddc3bc5153305a590cb30
Steve Bogucki <sbogucki@mail.usf.edu>
2019-01-03 14:26:23 -0800

    wiring up redux on vdrlogin

---
+++ client/src/reducers/userReducer.js
@@ -0,0 +1,10 @@
+import { DETERMINE_USER } from '../actions/types';
+
+export default function (state = null, action){
+    switch (action.type) {
+        case DETERMINE_USER:
+            return action.payload || false; 
+        default:
+            return state; 
+    }
+}
